7fa5fbe63da219d721803deea9fa9ef363a4fd24
Lucas Conte <144933020+lucascontee@users.noreply.github.com>
2023-12-03 13:08:41 -0300

    alterando as condições
    Alterei as condições para que não fosse possível apresentar resultados negativos, também alterei o :focus dos inputs para criar uma borda verde

--- script.js
+++ script.js
@@ -16,21 +16,32 @@ function clicar(){
     var mes = Number(mes.value)
     var ano = Number(ano.value)
 
-    if(mes == 0){
+    if(mes == 0 || mes > 12){
         window.alert("Mês inválido")
-    } else if(dia == 0){
-        window.alert("Digite um dia")
-    } else if(ano == 0 && ano > 2023){
+    } else if(dia <= 0 || dia > 31){
+        window.alert("Digite um dia válido")
+    } else if(ano == 0 || ano > 2023){
         window.alert("Ano inválido")
     } else{
         var resy = a - ano
         var resd = d - dia
         var resm = m - mes
-        
-        ar.innerText = resy
+
+        if(d < dia){
+            resm = m - mes - 1
+            resd = 30 + d - dia
+        }
+
         mr.innerHTML = resm
+        ar.innerText = resy
         dr.innerHTML = resd
-        
+
+        if(resm < 10){
+            mr.innerHTML = "0" + resm
+        }
+
+        if(resd < 10)
+        dr.innerHTML = "0"+resd
         
     }
 }
